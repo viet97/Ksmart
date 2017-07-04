b9257045e3aa2d309f331a4633540de6efc481b2
DESKTOP-05I134S\Quoc Viet Dang <quocvietdang97@gmail.com>
2017-07-04 15:14:13 +0700

    newfeed

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -5,6 +5,8 @@ import Icon2 from 'react-native-vector-icons/Entypo'
 import Icon3 from 'react-native-vector-icons/FontAwesome'
 import React from 'react';
 import Drawer from 'react-native-drawer';
+import Color from '../configs/color'
+
 import {
     AppRegistry,
     Text,
@@ -20,6 +22,7 @@ import Header from "./Header";
 var {height} = Dimensions.get('window');
 export default class HomeScreen extends React.Component {
     static navigationOptions = {
+
         header: null
 
     };
@@ -40,11 +43,13 @@ export default class HomeScreen extends React.Component {
     constructor(props) {
         super(props)
         this.state = ({
+            gestureName: 'none',
             width: 35,
             height: 35,
             status: true,
             selectedTab: 'NewFeed',
             headerTitleStyle: {alignSelf: 'center'},
+            dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
         })
     }
 
@@ -68,20 +73,37 @@ export default class HomeScreen extends React.Component {
     newFeedScreen() {
         return (
 
-            <View >
+            <View style={{flex: 1}}>
                 <View style={styles.titleStyle}>
                     {this.isToogleVisible()}
-                    <Text style={{fontSize: 20}}>NewFeed</Text>
-                    <View style={{backgroundColor: 'white', width: 35, height: 35}}></View>
+                    <Text style={{fontSize: 20, color: 'white'}}>NewFeed</Text>
+                    <View style={{backgroundColor: Color.backgroundNewFeed, width: 35, height: 35}}></View>
                 </View>
 
                 <TouchableOpacity onPress={() => this.openControlPanel()}
                                   style={{width: 35, height: 35, position: 'absolute'}}/>
+                <View style={{backgroundColor: Color.backgroundNewFeed, flex: 9}}>
+                    <ListView
+                        style={{backgroundColor:Color.itemListViewColor}}
+                        dataSource={this.state.dataSource}
+                        renderRow={(rowData) => {this.itemListView(rowData)}}
+                    />
+                </View>
             </View>
 
         )
     }
-
+    itemListView(data){
+        return(
+            <View style={{flexDirection:'row',height:height/6,flex:1}}>
+                <Image source={{uri:data.url}} style={{flex:1 }}/>
+                <View style={{flex:5}}>
+                    <Text style={{fontSize:15,color:Color.itemNameListViewColor}}>{data.name}</Text>
+                    <Text style={{fontSize:10,color:'white'}}> {data.action}</Text>
+                </View>
+            </View>
+        )
+    }
     menuScreen() {
         return (
             <View >
@@ -90,12 +112,13 @@ export default class HomeScreen extends React.Component {
                         source={require('../images/MenuBar.png')}
                         style={{width: 35, height: 35}}/>
                     <Text style={{fontSize: 20}}>Menu</Text>
-                    <View style={{backgroundColor: 'white', width: 35, height: 35}}></View>
+                    <View style={{width: 35, height: 35}}></View>
                 </View>
                 <TouchableOpacity onPress={() => this.openControlPanel()}
                                   style={{width: 35, height: 35, position: 'absolute'}}/>
                 <View style={{width: 100, height: 100, elevation: 20}}>
-                    <Icon2  style ={{ alignSelf:'center',borderRadius:20,fontSize:100,backgroundColor:'green'}}size={24} color="black" name="user"/>
+                    <Icon2 style={{alignSelf: 'center', borderRadius: 20, fontSize: 100, backgroundColor: 'green'}}
+                           size={24} color="black" name="user"/>
                 </View>
             </View>
         )
@@ -105,11 +128,12 @@ export default class HomeScreen extends React.Component {
         return (
             <View>
                 <View style={{justifyContent: 'center', height: 50, backgroundColor: '#570A60', elevation: 15}}>
-                    <Image source={require('../images/bg.png')} style={{flex:1}}/>
-                    <Image source={require('../images/logoksmart.png')} style={{alignSelf:'center',position:'absolute'}}/>
+                    <Image source={require('../images/bg.png')} style={{flex: 1}}/>
+                    <Image source={require('../images/logoksmart.png')}
+                           style={{alignSelf: 'center', position: 'absolute'}}/>
                 </View>
-                <View style={{paddingTop: 15,flexDirection:'column'}}>
-                    <Image style={{position:'absolute'}} source={require('../images/bg.png')}/>
+                <View style={{paddingTop: 15, flexDirection: 'column'}}>
+                    <Image style={{position: 'absolute'}} source={require('../images/bg.png')}/>
                     <View style={styles.itemSideMenuStyle}>
                         <Icon1 size={24} color="black" name="ios-people-outline"/>
                         <Text style={{marginLeft: 10}}>Quản lý nhân viên</Text>
@@ -154,7 +178,7 @@ export default class HomeScreen extends React.Component {
     MainView() {
         return (
 
-            <TabNavigator tabBarStyle={{backgroundColor: '#FBBF45'}}>
+            <TabNavigator tabBarStyle={{backgroundColor: Color.tabbarColor}}>
                 <TabNavigator.Item
                     selected={this.state.selectedTab === 'NewFeed'}
                     title="NewFeed"
@@ -179,6 +203,23 @@ export default class HomeScreen extends React.Component {
         )
     }
 
+    componentDidMout() {
+        var arr = [
+            {   name:'Đặng Quốc Việt',
+                 url:'https://scontent.fhan2-1.fna.fbcdn.net/v/t1.0-1/p160x160/1936235_1039707076102011_5958200926723814588_n.jpg?oh=95e45933baccb02bf6a965fe4ce4b2fe&oe=5A0728F5',
+                 action:'Vừa Đăng nhập 10 phút trước'},
+            {   name:'Hoàng trần Hoảng',
+                url:'https://scontent.fhan2-1.fna.fbcdn.net/v/t1.0-9/12369200_552344928266177_708527824307797917_n.jpg?oh=93ad512f12d70a1ed802fb8df6e03c85&oe=59CB9F2C',
+                action:'Vừa Đăng nhập 10 phút trước'},
+            {   name:'Nguyễn Đức Thắng',
+                url:'https://scontent.fhan2-1.fna.fbcdn.net/v/t1.0-9/12369200_552344928266177_708527824307797917_n.jpg?oh=93ad512f12d70a1ed802fb8df6e03c85&oe=59CB9F2C',
+                action:'Vừa Đăng nhập 10 phút trước'},
+            {   name:'Trần Hán Hiếu',
+                url:'https://scontent.fhan2-1.fna.fbcdn.net/v/t1.0-9/17799998_1949714765273125_6206086656656546584_n.jpg?oh=91e183590a290e6613918272f8c4ea4c&oe=59D9778D',
+                action:'Vừa Đăng nhập 10 phút trước'}]
+        
+    }
+
     render() {
         return (
 
@@ -186,7 +227,8 @@ export default class HomeScreen extends React.Component {
                 ref={(ref) => this._drawer = ref}
                 tapToClose={true}
                 openDrawerOffset={0.4}
-                onClose={() => this.showToogle()}
+                negotiatePan={true}
+                onCloseStart={() => this.showToogle()}
                 content={this.sideMenuView()}>
                 {this.MainView()}
             </Drawer>
@@ -197,19 +239,20 @@ export default class HomeScreen extends React.Component {
 }
 const styles = StyleSheet.create({
     titleStyle: {
+        flex: 1,
         elevation: 15,
         justifyContent: 'space-between',
         flexDirection: 'row',
-        backgroundColor: 'white',
+        backgroundColor: Color.backgroundNewFeed,
     },
     headerStyle: {
         elevation: 15, height: this.height / 7
     },
     itemSideMenuStyle: {
-        borderBottomWidth:0.5,
-        borderBottomColor:'white',
+        borderBottomWidth: 0.5,
+        borderBottomColor: 'white',
         flexDirection: 'row',
         margin: 10,
-        paddingBottom:8
+        paddingBottom: 8
     }
 })
---
+++ configs/color.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Quoc Viet Dang on 7/4/2017.
+ */
+export  default class Color{
+    static red='#123456'
+    static backgroundNewFeed='#5E5B95'
+    static tabbarColor='#4AD6BD'
+    static itemListViewColor='#6C69A4'
+    static itemNameListViewColor='#6AC7F1'
+}
